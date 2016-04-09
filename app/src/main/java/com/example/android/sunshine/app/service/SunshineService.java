581c8d9b993dc8a1972115033ef3bcd3a64b88d2
Kerry Dong <kerry50407@gmail.com>
2016-04-09 17:56:30 +0800

    Create sunshine service (not used yet)

---
+++ app/src/main/java/com/example/android/sunshine/app/service/SunshineService.java
@@ -0,0 +1,19 @@
+package com.example.android.sunshine.app.service;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+/**
+ * Created by Kerry on 2016/4/9.
+ */
+public class SunshineService extends IntentService {
+
+    public SunshineService() {
+        super("SunshineService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+}
