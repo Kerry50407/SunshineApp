2280f26585b4da8515f31c5be14e684b4a326db2
Kerry Dong <kerry50407@gmail.com>
2016-04-26 15:54:19 +0800

    Material design layout - Use card view part 2

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -117,7 +117,7 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
 
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-        mListView = (ListView) rootView.findViewById(R.id.listView_forecast);
+        mListView = (ListView) rootView.findViewById(R.id.listview_forecast);
         View emptyView = rootView.findViewById(R.id.listview_forecast_empty);
         mListView.setEmptyView(emptyView);
         mListView.setAdapter(mForecastAdapter);
